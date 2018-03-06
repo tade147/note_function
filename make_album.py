207bbd32ba85abc2cdc58c4cd04f678667e1038b
tade147 <36809084+tade147@users.noreply.github.com>
2018-03-06 22:20:51 +0800

    judge

--- make_album.py
+++ make_album.py
@@ -1,9 +1,20 @@
 def make_album(singer, album_name):
     """存储歌手及专辑名"""
-    album = {'singer': singer, 'album': album_name}
+    album = {'musician' : singer, 'album' : album_name}
     return album
 
-m_album = make_album('黄龄', '痒')
-print(m_album)
+while True:
+    print("\n请输入歌手名字:")
+    print("请输入专辑名字：")
+    print("你可以输入'退出'来退出程序。")
+    musician = input("歌手名字: ")
+    if musician == '退出':
+        break
+
+    album = input("专辑名称: ")
+    if album == '退出':
+        break
+    ma = make_album(musician, album)
+    print(ma)
 
     
