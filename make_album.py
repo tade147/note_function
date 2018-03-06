e5363ba0c759249cd9a83d2de0a4a3462ea8c83d
tade147 <36809084+tade147@users.noreply.github.com>
2018-03-06 20:29:36 +0800

    Create make_album.py

---
+++ make_album.py
@@ -0,0 +1,9 @@
+def make_album(singer, album_name):
+    """存储歌手及专辑名"""
+    album = {'singer': singer, 'album': album_name}
+    return album
+
+m_album = make_album('黄龄', '痒')
+print(m_album)
+
+    
