d4df75509353ab2156f6f744847d240ab40f4c6c
tade147 <36809084+tade147@users.noreply.github.com>
2018-03-06 20:32:50 +0800

    Update make_album.py

--- make_album.py
+++ make_album.py
@@ -1,9 +1,9 @@
-def make_album(singer, album_name):
+def make_album(singer, album_name, number= '12'):
     """存储歌手及专辑名"""
-    album = {'singer': singer, 'album': album_name}
+    album = {'singer': singer, 'album': album_name, 'number' : number}
     return album
 
-m_album = make_album('黄龄', '痒')
+m_album = make_album('黄龄', '痒', )
 print(m_album)
 
     
